9113d722fe8f8c402fd5ec36f91c10ecfb47d9f3
Keinar Elkayam <keinarelkayam@gmail.com>
2025-11-07 13:02:02 +0200

    Add user authentication with registration and login
    Introduced user registration and login endpoints with JWT authentication. Added user model with password hashing, user controller for handling authentication logic, and user routes. Updated server to use new routes and required dependencies (bcryptjs, express-async-handler, jsonwebtoken) in package files.

---
+++ backend/controllers/userController.js
@@ -0,0 +1,78 @@
+const asyncHandler = require('express-async-handler');
+const User = require('../models/userModel');
+const bycrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const generateToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: '30d',
+    });
+};
+
+// @desc    Register new user
+// @route   POST /api/users/register
+// @access  Public
+const registerUser = asyncHandler(async (req, res) => {
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+        res.status(400);
+        throw new Error('Please fill all fields');
+    }
+
+    const userExists = await User.findOne({ email });
+
+    if (userExists) {
+        res.status(400);
+        throw new Error('User already exists');
+    }
+
+    const user = await User.create({
+        name,
+        email,
+        password,
+        isAdmin: true,
+    });
+
+    if (user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user._id),
+        });
+    } else {
+        res.status(400);
+        throw new Error('Invalid user data');
+    }
+});
+
+// @desc    Authenticate a user
+// @route   POST /api/users/login
+// @access  Public
+const loginUser = asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (user && (await bycrypt.compare(password, user.password))) {
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user._id),
+        });
+    } else {
+        res.status(401);
+        throw new Error('Invalid email or password');
+    }
+});
+
+module.exports = {
+    registerUser,
+    loginUser,
+};
+
+
--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,21 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
+const userRoutes = require('./routes/userRoutes');
+
 
 dotenv.config();
 connectDB();
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
     res.send('API is running...');
 });
+
+app.use('/api/users', userRoutes);
 
 const PORT = process.env.PORT || 5001;
 
